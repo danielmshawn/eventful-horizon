4e70b810526678554c49a21a0583ad0d4b30f4c5
danielmshawn <danielmshawn@gmail.com>
2023-07-30 21:07:10 -0700

    File organization changes, initial setup, changes to App.jsx and index.js

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,17 @@
+
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+
+export default function App() {
+
+  return (
+    <div className="App">
+        <p>
+         This will one day be EVENTFUL HORIZON YAAAAY
+        </p>
+       
+    </div>
+  );
+}
+
