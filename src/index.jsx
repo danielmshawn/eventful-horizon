7dbb5d070cb8684c710ec93ccbf5fdf6d1afe03e
danielmshawn <danielmshawn@gmail.com>
2023-08-02 22:04:53 -0700

    Undo react google oauth package, now using google identity services. New script in index.html, additions in App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './pages/App/App'
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log('Google Client ID:', process.env.REACT_APP_GOOGLE_CLIENT_ID);
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    {/* <GoogleOAuthProvider
+      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+      scope="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar"
+    > */}
       <Router>
         <App />
       </Router>
-    </GoogleOAuthProvider >
+    {/* </GoogleOAuthProvider > */}
   </React.StrictMode>
 );
 
