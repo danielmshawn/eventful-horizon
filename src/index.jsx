081c7b36ce02d3bf9c32541e516ec6bdd33361c3
danielmshawn <danielmshawn@gmail.com>
2023-08-02 23:13:09 -0700

    Sign in/out functionality. Optional google one tap dialogue current active.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './pages/App/App'
 import reportWebVitals from './reportWebVitals';
@@ -11,14 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log('Google Client ID:', process.env.REACT_APP_GOOGLE_CLIENT_ID);
 root.render(
   <React.StrictMode>
-    {/* <GoogleOAuthProvider
-      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-      scope="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar"
-    > */}
       <Router>
         <App />
       </Router>
-    {/* </GoogleOAuthProvider > */}
   </React.StrictMode>
 );
 
